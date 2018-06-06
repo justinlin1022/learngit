4275f4bce49785b0e45c7cf1cff71889f9a430f9
melvinlin <justinlin1022@gmail.com>
2018-06-06 23:58:49 +0800

    modify test.js in dev branch

--- cfg/test.js
+++ cfg/test.js
@@ -3,3 +3,4 @@ l'd like to modify this file to test git status
 modify this file again
 creating a new branch is quick.
 creating a new branch is quich for merge about master and feature1 branch.
+modify this file for dev branch.
