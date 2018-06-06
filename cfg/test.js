b78708949eda421edd2a9dff27c528f80a0a682b
melvinlin <justinlin1022@gmail.com>
2018-06-07 00:11:11 +0800

    modify test.js for stash

--- cfg/test.js
+++ cfg/test.js
@@ -4,3 +4,4 @@ modify this file again
 creating a new branch is quick.
 creating a new branch is quich for merge about master and feature1 branch.
 modify this file for dev branch.
+modify this file for stash.
